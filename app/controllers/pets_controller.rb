dee703217316dc406b521ba831336b818b81f0fe
Tarleton Moore <tarletonmoore@Tarletons-MacBook-Pro.local>
2023-08-15 14:45:56 -0400

    add full crud to pets

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -6,4 +6,45 @@ class PetsController < ApplicationController
   def show
     @pet = Pet.find(params[:id])
   end
+
+  def new
+    @pet = Pet.new
+  end
+
+  def create
+    @pet = Pet.new(pet_params)
+
+    if @pet.save
+      redirect_to @pet
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @pet = Pet.find(params[:id])
+  end
+
+  def update
+    @pet = Pet.find(params[:id])
+
+    if @pet.update(pet_params)
+      redirect_to @pet
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @pet = Pet.find(params[:id])
+    @pet.destroy
+
+    redirect_to root_path, status: :see_other
+  end
+
+  private
+
+  def pet_params
+    params.require(:pet).permit(:name, :breed, :image_url)
+  end
 end
--- app/models/pet.rb
+++ app/models/pet.rb
@@ -1,2 +1,5 @@
 class Pet < ApplicationRecord
+  validates :name, presence: true
+  validates :breed, presence: true
+  validates :image_url, presence: true
 end
